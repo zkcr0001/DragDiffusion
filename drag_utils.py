c7ef5152798cad1f48390fbe6f5caaea762bdee2
zkcr0001 <zhengkcr@gmail.com>
2023-08-11 20:34:06 +0000

    add drag diffusion translation

--- drag_utils.py
+++ drag_utils.py
@@ -127,3 +127,66 @@ def drag_diffusion_update(model, init_code, t, handle_points, target_points, mas
         optimizer.zero_grad()
 
     return init_code, text_emb
+
+
+def drag_diffusion_update_region(model, init_code, t, handle_points, target_points, mask, args):
+
+    assert len(handle_points) == len(target_points), \
+        "number of handle point must equals target points"
+
+    text_emb = model.get_text_embeddings(args.prompt).detach()
+    # the init output feature of unet
+    with torch.no_grad():
+        unet_output, F0 = model.forward_unet_features(init_code, t, encoder_hidden_states=text_emb,
+            layer_idx=args.unet_feature_idx, interp_res=args.sup_res)
+        x_prev_0, _ = model.step(unet_output, t, init_code) # 返回 x_prev 和 pred_x0
+
+    # prepare optimizable init_code and optimizer
+    init_code.requires_grad_(True)
+    optimizer = torch.optim.Adam([init_code], lr=args.lr)
+
+    # prepare for point tracking and background regularization
+    handle_points_init = copy.deepcopy(handle_points)
+    interp_mask = F.interpolate(mask, (init_code.shape[2],init_code.shape[3]), mode='nearest')
+
+    # prepare amp scaler for mixed-precision training
+    scaler = torch.cuda.amp.GradScaler()
+    for step_idx in range(args.n_pix_step):
+        with torch.autocast(device_type='cuda', dtype=torch.float16):
+            unet_output, F1 = model.forward_unet_features(init_code, t, encoder_hidden_states=text_emb,
+                layer_idx=args.unet_feature_idx, interp_res=args.sup_res)
+            x_prev_updated, _ = model.step(unet_output, t, init_code)
+
+            # do point tracking to update handle points before computing motion supervision loss
+            if step_idx != 0:
+                handle_points = point_tracking(F0, F1, handle_points, handle_points_init, args)
+                print('new handle points', handle_points)
+
+            # break if all handle points have reached the targets
+            if check_handle_reach_target(handle_points, target_points):
+                break
+
+            loss = 0.0
+            for i in range(len(handle_points)):
+                pi, ti = handle_points[i], target_points[i]
+                # skip if the distance between target and source is less than 1
+                if (ti - pi).norm() < 1:
+                    continue
+
+                di = (ti - pi) / (ti - pi).norm()
+
+                # motion supervision
+                f0_patch = F1[:,:,int(pi[0])-args.r_m:int(pi[0])+args.r_m+1, int(pi[1])-args.r_m:int(pi[1])+args.r_m+1].detach()
+                f1_patch = interpolate_feature_patch(F1, pi[0] + di[0], pi[1] + di[1], args.r_m)
+                loss += ((2*args.r_m+1)**2)*F.l1_loss(f0_patch, f1_patch)
+
+            # masked region must stay unchanged
+            loss += args.lam * ((x_prev_updated-x_prev_0)*(1.0-interp_mask)).abs().sum()
+            print('loss total=%f'%(loss.item()))
+
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        optimizer.zero_grad()
+
+    return init_code, text_emb
